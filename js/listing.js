d530ca5ffe0fa4b24cd1cf13fa418daccc2376c1
gordanakis <ncgordan@gmail.com>
2019-09-18 17:40:07 +0200

    Update listing.js

--- js/listing.js
+++ js/listing.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
                     { searchable: false, "targets": 'dt-nosearch' },
                     { width: 600, targets: [3] },
                     { width: 300, targets: [4] },
-                    { className: "crema", "targets": [ 8, 12 ] },
+                    { className: "nata", "targets": [ 8, 12 ] },
                     { className: "trufa", "targets": [ 7, 11 ] },
-                    { className: "nata", "targets": [ 6, 10 ] },
+                    { className: "crema", "targets": [ 6, 10 ] },
 
          
                 ]
@@ -18,16 +18,18 @@ $(document).ready(function(){
 
         var pedidos_table = $('#pedidos').DataTable({
             //"dom": '<"top"i>rt<"bottom"flp><"clear">',
-            /*
+            
             scrollY:        "60vh",
             scrollX:        true,
-            scrollCollapse: true,        
-            fixedColumns: true,
-            */
+            scrollCollapse: true,     
+			
+            //fixedColumns: true,
+            /*
             fixedHeader: {
                 header: true,
                 headerOffset: $('#fixed').height()
             },
+			*/
             ajax: "./json/pedidos.json",
         });
     });
