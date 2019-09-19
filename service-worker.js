dbd34b1f862c9619e0e467a0178f4ce6f2599c11
gordanakis <ncgordan@gmail.com>
2019-09-20 00:26:00 +0200

    v6

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,66 @@
-cacheName = 'my-cache';
-    const filestoCache = [
-      '/index.html',
-      '/css/home-style.css',
-      '/js/listing.js',
-      '/img/pattern2b.png'
-	  '/img/logo.png'
-    ]; 
-    self.addEventListener('install', e => {
-      e.waitUntil(
-        caches.open(cacheName)
-        .then(cache => cache.addAll(filesToCache))
-      );
-    });
+self.addEventListener('install', (event) => {
+    console.log(event);
+});
+
+self.addEventListener('activate', (event) => {
+    console.log(event);
+});
+/*
+	'/',
+	'/index.html',
+	'/css/home-style.css',
+	'/js/listing.js',
+	'/img/pattern2b.png',
+	'/img/logo.png',
+*/
+self.addEventListener('fetch', (event) => {
+	/*
+	 // Ignore crossdomain requests
+	 if (!event.request.url.startsWith(self.location.origin)) {
+	   return;
+	 }
+	 // Ignore non-GET requests
+	 if (event.request.method !== 'GET') {
+	   return;
+	 }
+	 // Ignore browser-sync
+	 if (event.request.url.indexOf('browser-sync') > -1) {
+	   return;
+	 }
+	 // Prevent index route being cached
+	 if (event.request.url === (self.location.origin + '/')) {
+	   return;
+	 }
+	 // Prevent index.html being cached
+	 if (event.request.url.endsWith('index.html')) {
+	   return;
+	 }
+	 */
+	console.log(event);
+ 
+	const assetsCacheName = 'v1-assets';
+
+	// Tell the fetch to respond with this chain
+	event.respondWith(
+	   // Open the cache
+	   caches.open(assetsCacheName)
+		 .then((cache) => {
+		   // Look for matching request in the cache
+		   return cache.match(event.request)
+			 .then((matched) => {
+			   // If a match is found return the cached version first
+			   if (matched) {
+				 return matched;
+			   }
+			   // Otherwise continue to the network
+			   return fetch(event.request)
+				 .then((response) => {
+				   // Cache the response
+				   cache.put(event.request, response.clone());
+				   // Return the original response to the page
+				   return response;
+				 });
+			 });
+		 })
+	);
+});
